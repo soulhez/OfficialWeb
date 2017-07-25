ad6890a00c0c20dc844c0aba69845a5c95e0f71d
undwin <love@und.win>
2017-07-25 21:04:05 +0800

    注释

--- src/test/Demo.java
+++ src/test/Demo.java
@@ -3,6 +3,6 @@ package test;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("1");
-		System.out.println("Ðì¾ü");sdjkfljlk
+		System.out.println("Ðì¾ü");
 	}
 }
