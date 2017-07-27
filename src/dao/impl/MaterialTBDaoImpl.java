0a76fdfa3fec60a0b9fadb300614d7aeffd3c6f6
xujunGitHub <466465433@qq.com>
2017-07-27 17:07:12 +0800

    真正的实现类定义完成

--- src/dao/impl/MaterialTBDaoImpl.java
+++ src/dao/impl/MaterialTBDaoImpl.java
@@ -1,38 +1,35 @@
 package dao.impl;
 
+
 import java.util.List;
 
 import dao.BaseDao;
-import dao.ArticleTypeTBDao;
-import entity.ArticleTypeTB;
+import dao.MaterialTBDao;
+import entity.MaterialTB;
 
-
-
-public class MaterialTBDaoImpl extends BaseDao implements ArticleTypeTBDao {
-
-	public int addArticleTypeTB(ArticleTypeTB articleTypeTB) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public int deleteArticleTypeTB(String aId) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public List<ArticleTypeTB> searchArticleTypeTB(String aNvntitleTBid) {
-		// TODO Auto-generated method stub
+/**
+ * 素材实现类
+ * @author Administrator
+ *
+ */
+public class MaterialTBDaoImpl extends BaseDao implements MaterialTBDao {
+	/**
+	 * 根据素材类型查询此类型的素材集合
+	 */
+	public List<String> searchMaterialTB(String mType) {
 		return null;
 	}
-
-	public ArticleTypeTB searchArticleTypeTBByArticleTBId(String aID) {
-		// TODO Auto-generated method stub
+	/**
+	 * 根据素材类型查询素材名称
+	 */
+	public String searchMaterialTBmName(String mType) {
 		return null;
 	}
-
-	public int updateArticleTypeTB(String aId, ArticleTypeTB articleTypeTB) {
-		// TODO Auto-generated method stub
+	/**
+	 * 根据素材ID修改素材
+	 */
+	public int updateMaterialTB(String mId, MaterialTB materialTB) {
 		return 0;
 	}
-
+	
 }
