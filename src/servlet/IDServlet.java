1cafada84188a9970e5a730d305db341abe7e8de
undwin <love@und.win>
2017-07-29 17:39:31 +0800

    编写两个servlet

--- src/servlet/IDServlet.java
+++ src/servlet/IDServlet.java
@@ -1,11 +1,23 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import dao.impl.ArticleTBDaoImpl;
+import dao.impl.ArticleTypeTBDaoImpl;
+
+import entity.AdminTB;
+import entity.ArticleTB;
+import entity.ArticleTypeTB;
 
 public class IDServlet extends HttpServlet {
 	
@@ -20,20 +32,37 @@ public class IDServlet extends HttpServlet {
 			request.setCharacterEncoding("utf-8");
 			response.setContentType("text/html; charset=utf-8");
 			String method=request.getParameter("method");
+			String id=(String)request.getAttribute("id");
+			String start=request.getParameter("start");
+			String end=request.getParameter("end");
+			
 			//根据文章类型ID查询文章后，以文章创建时间降序排列
+			PrintWriter out=response.getWriter();
 			 if(method.equalsIgnoreCase("searchArticleTB")){
-				
+				 List<ArticleTB> list=new ArticleTBDaoImpl().searchArticleTB(id, start, end);
+				 JSONArray ja=new JSONArray().fromObject(list);
+				 out.write(ja.toString());
+				 return;
 			}//根据文章ID查询文章
 			else if(method.equalsIgnoreCase("searchArticleTBByID")){
-				
+				ArticleTB at=new  ArticleTBDaoImpl().searchArticleTBByID(id);
+				JSONObject json=new JSONObject().fromObject(at);
+				out.write(json.toString());
+				return;
 			}
 			 //根据导航ID查询文章类型集合
 			else if(method.equalsIgnoreCase("searchArticleTypeTB")){
-				
+				 List<ArticleTypeTB> list=new ArticleTypeTBDaoImpl().searchArticleTypeTB(id);
+				 JSONArray ja=new JSONArray().fromObject(list);
+				 out.write(ja.toString());
+				 return;
 			}
 			 //获取管理员信息
 			else{
-				
+				AdminTB admin=(AdminTB)request.getSession().getAttribute("admin");
+				JSONObject json=new JSONObject().fromObject(admin);
+				out.write(json.toString());
+				return;
 			}
 	}
 }
