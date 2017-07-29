cb62d0fd1a2b6586d0b38cd1517dd9cbf341854a
undwin <love@und.win>
2017-07-29 14:48:13 +0800

    你妹的

--- src/dao/BaseDao.java
+++ src/dao/BaseDao.java
@@ -13,7 +13,7 @@ public class BaseDao {
 	private static final String DRIVER="com.mysql.jdbc.Driver";
 	private static final String URL="jdbc:mysql://localhost/officialwebdb";
 	private static final String USER="root";
-	private static final String PWD="mysqlok";
+	private static final String PWD="ok";
 	protected Connection con=null;
 	protected PreparedStatement pstmt=null;
 	protected ResultSet rs=null;
